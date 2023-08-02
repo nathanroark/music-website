a1d8881a8d8f8871a187cb37449e96cb98796938
Nathan Roark <nathanroark@protonmail.com>
2023-08-02 16:21:23 -0500

    some progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,23 @@ import Footer from '@/components/Footer';
 import getPostMetadata from '../components/getPostMetadata';
 import PostPreview from '../components/PostPreview';
 import SortBar from '@/components/SortBar';
-export default function Home({ searchParams }: { searchParams?: { [key: string]: string | undefined }; }) {
+export default function Home({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | undefined };
+}) {
   const sortMethod = searchParams?.sort || '';
   const postMetadata = getPostMetadata(sortMethod);
   const postPreviews = postMetadata.map((post) => <PostPreview key={post.slug} {...post} />);
-
 
   // TODO: Add Brave Bird
   // TODO: Add Chelseas Wierd
 
   return (
-    <div className="flex justify-center items-center flex-col pt-[3rem]">
+    <div className="flex justify-center items-center flex-col pt-[3rem] pl-[14rem]">
       <SortBar />
       <div className="flex justify-center">
-        <div className="max-w-7xl container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <div className="grid gap-2 md:gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 ">
             {postPreviews}
           </div>
--- src/components/SortBar.tsx
+++ src/components/SortBar.tsx
@@ -1,42 +1,54 @@
 import Link from 'next/link';
-const SortBar = ({ searchParams }: { searchParams?: { [key: string]: string | undefined }; }) => {
+
+const SortBar = ({ searchParams }: { searchParams?: { [key: string]: string | undefined } }) => {
+  console.log('searchParams', searchParams);
   const sortMethod = searchParams?.sort || '';
+  const SortItems = [
+    {
+      name: 'Post Date',
+      query: '',
+    },
+    {
+      name: 'Artist A to Z',
+      query: 'artist_a-z',
+    },
+    {
+      name: 'Artist Z to A',
+      query: 'artists_z-a',
+    },
+    {
+      name: 'Release Date',
+      query: 'release_date',
+    },
+    {
+      name: 'Reverse Release Date',
+      query: 'release_date_reverse',
+    },
+  ];
+
   return (
-    <div className='absolute top-0 left-0 w-fit p-4 bg-black border-r border-zinc-600 pt-[3rem] h-screen mt-2 rounded-md'>
-      <h3 className='text-2xl font-bold text-gray-200 underline'>Sort By</h3>
-      <ul className='flex flex-col gap-2'>
-        <div className="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
-          <input id="bordered-radio-1" type="radio" value="" name="bordered-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-          <label className="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default radio</label>
-        </div>
-        <div className="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
-          <input id="bordered-radio-2" type="radio" value="" name="bordered-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-          <label className="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Checked state</label>
-        </div>
-        <li>
+    <div className="absolute top-0 left-0 w-[14rem] p-4 bg-black border-r border-zinc-600 pt-[3rem] h-screen mt-2 rounded-md">
+      {SortItems.map((item, index) => {
+        return (
           <Link
+            key={index}
             href={{
-              pathname: "/",
-              query: { sort: "artist a-z" }
+              pathname: '/',
+              query: { sort: item.query },
             }}
-            className='flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700'
+            className="w-full flex justify-center items-center bg-zinc-900  hover:bg-zinc-800 
+                border border-zinc-400 rounded duration-300 ease-in-out relative py-2"
           >
-            <input id="bordered-radio-1" type="radio" checked value="" name="bordered-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-            <label className="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">A - Z</label>
+            <span className="relative">
+              {item.query === sortMethod ? (
+                <span className="ml-2 text-green-400">{item.name}</span>
+              ) : (
+                <span className="ml-2 text-zinc-100">{item.name}</span>
+              )}
+            </span>
           </Link>
-        </li>
-        <li>
-          <Link
-            href={{
-              pathname: "/",
-              query: { sort: "release_date" }
-            }}
-            className='p-2 m-2 bg-emerald-700 text-lg font-medium text-gray-950 rounded-md'
-          >
-            Release Date
-          </Link>
-        </li>
-      </ul>
+        );
+      })}
     </div>
   );
 };
