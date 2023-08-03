1055aba2744f997cb79daa853ed773b45f5fe479
Nathan Roark <nathanroark@protonmail.com>
2023-08-03 18:05:18 -0500

    made more changes to sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,106 +1,104 @@
-"use client";
-import Link from "next/link";
-import { usePathname, useSearchParams } from "next/navigation";
-import { Fragment } from "react";
-import { Menu, Transition } from "@headlessui/react";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
-import clsx from "clsx";
+'use client';
+import Link from 'next/link';
+import { usePathname, useSearchParams } from 'next/navigation';
+import { Fragment } from 'react';
+import { Menu, Transition } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
+import clsx from 'clsx';
 
 const Sidebar = () => {
-  let pathname = usePathname() || "/";
-  if (pathname != "/") {
+  const searchParams = useSearchParams();
+
+  let pathname = usePathname() || '/';
+  if (pathname != '/') {
     return null;
   }
-  const searchParams = useSearchParams();
-  const sortMethod = searchParams.get("sort") || "";
-  const filterMethod = searchParams.get("filterMethod") || "";
+  const sortMethod = searchParams.get('sort') || '';
+  const filterMethod = searchParams.get('filterMethod') || 'some';
+  const filters: string = searchParams.get('filters') || '';
 
-  const filters: string = searchParams.get("filters") || "";
+  const currentFilters = filters.split(',').filter((filter) => filter !== '');
 
-  const currentFilters = filters.split(",").filter((filter) => filter !== "");
-  console.log("Sidebar -> currentFilters", currentFilters);
   const SortItems = [
     {
-      name: "Post Date",
-      query: "",
+      name: 'Post Date',
+      query: '',
     },
     {
-      name: "Artist A to Z",
-      query: "artist_alphabetical",
+      name: 'Artist A to Z',
+      query: 'artist_alphabetical',
     },
     {
-      name: "Artist Z to A",
-      query: "artist_reverse_alphabetical",
+      name: 'Artist Z to A',
+      query: 'artist_reverse_alphabetical',
     },
     {
-      name: "Release Date",
-      query: "release_date",
+      name: 'Release Latest',
+      query: 'release_date',
     },
     {
-      name: "Reverse Release Date",
-      query: "release_date_reverse",
+      name: 'Release Oldest',
+      query: 'release_date_reverse',
     },
   ];
 
-  const FilterMethods = ["some", "every"];
+  const FilterMethods = ['some', 'every'];
 
   const genreList = [
-    "Midwest Emo",
-    "Screamo",
-    "Shoegaze",
-    "Math Rock",
-    "Post Hardcore",
-    "Post Rock",
-    "Indie Rock",
+    'Midwest Emo',
+    'Screamo',
+    'Shoegaze',
+    'Math Rock',
+    'Post Hardcore',
+    'Post Rock',
+    'Indie Rock',
   ];
 
-  const appliedFilters = genreList.filter((genre) =>
-    currentFilters.includes(genre)
-  );
+  const appliedFilters = genreList.filter((genre) => currentFilters.includes(genre));
 
-  const availableFilters = genreList.filter(
-    (genre) => !currentFilters.includes(genre)
-  );
+  const availableFilters = genreList.filter((genre) => !currentFilters.includes(genre));
 
   return (
     <div className="fixed top-0 left-0 w-[14rem] p-4 flex flex-col gap-10 bg-black border-r border-zinc-600 pt-[3rem] h-screen mt-2 rounded-md">
       <div>
         <h2 className="text-2xl text-zinc-100 font-bold mb-4">Sort Method</h2>
-        {SortItems.map((item, index) => {
-          return (
-            <Link
-              key={index}
-              href={{
-                pathname: "/",
-                query: {
-                  sort: item.query,
-                  filters: filters,
-                  filterMethod: filterMethod,
-                },
-              }}
-              className="w-full flex justify-center items-center bg-zinc-900  hover:bg-zinc-800 
+        <div className="flex flex-col gap-1">
+          {SortItems.map((item, index) => {
+            return (
+              <Link
+                key={index}
+                href={{
+                  pathname: '/',
+                  query: {
+                    sort: item.query,
+                    filters: filters,
+                    filterMethod: filterMethod,
+                  },
+                }}
+                className="w-full flex justify-center items-center bg-zinc-900  hover:bg-zinc-800 
                 border border-zinc-400 rounded duration-300 ease-in-out relative py-2"
-            >
-              <span className="relative">
-                {item.query === sortMethod ? (
-                  <span className=" text-green-400">{item.name}</span>
-                ) : (
-                  <span className="text-zinc-100">{item.name}</span>
-                )}
-              </span>
-            </Link>
-          );
-        })}
+              >
+                <span className="relative">
+                  {item.query === sortMethod ? (
+                    <span className=" text-green-400">{item.name}</span>
+                  ) : (
+                    <span className="text-zinc-100">{item.name}</span>
+                  )}
+                </span>
+              </Link>
+            );
+          })}
+        </div>
       </div>
       <div>
         <h2 className="text-2xl text-zinc-100 font-bold mb-4">Filter Method</h2>
-        <div className="flex gap-4">
+        <div className="flex gap-2">
           {FilterMethods.map((item, index) => {
             return (
               <Link
                 key={index}
                 href={{
-                  pathname: "/",
+                  pathname: '/',
                   query: {
                     sort: sortMethod,
                     filters: filters,
@@ -122,40 +120,46 @@ const Sidebar = () => {
           })}
         </div>
       </div>
-      <div>
-        <h2 className="text-2xl text-zinc-100 font-bold mb-4">Filter</h2>
-        {appliedFilters.map((name, index) => {
-          return (
-            <div key={name + "selected genre"}>
-              <Link
-                // remove a genre from the filter list
-                href={{
-                  pathname: "/",
-                  query: {
-                    sort: sortMethod,
-                    filters: [...appliedFilters]
-                      .filter((genre) => genre !== name)
-                      .join(","),
-                    filterMethod: filterMethod,
-                  },
-                }}
-                className="w-full flex justify-center items-center bg-zinc-900  hover:bg-zinc-800 
-                border border-zinc-400 rounded duration-300 ease-in-out relative py-2"
-              >
-                <span className="relative">
-                  <span className="text-green-400">{name}</span>
-                </span>
-              </Link>
-            </div>
-          );
-        })}
+      <div className="flex flex-col gap-2">
+        <h2 className="text-2xl text-zinc-100 font-bold mb-2">Genres</h2>
+        <MyDropdown
+          availableFilters={availableFilters}
+          sortMethod={sortMethod}
+          appliedFilters={appliedFilters}
+          filterMethod={filterMethod}
+        />
+        <div className="flex flex-col gap-1">
+          {appliedFilters.map((name, index) => {
+            return (
+              <div key={name + 'selected genre'}>
+                <Link
+                  // remove a genre from the filter list
+                  href={{
+                    pathname: '/',
+                    query: {
+                      sort: sortMethod,
+                      filters: [...appliedFilters].filter((genre) => genre !== name).join(','),
+                      filterMethod: filterMethod,
+                    },
+                  }}
+                  className="w-full flex justify-center items-center bg-zinc-900  hover:bg-black 
+                border border-zinc-400 rounded duration-300 ease-in-out relative py-2 group"
+                >
+                  {/* <span className="invisible absolute left-4 group-hover:visible text-red-500">
+                  X
+                </span> */}
+
+                  <span className="relative ">
+                    <span className="text-zinc-300 group-hover:line-through group-hover:text-red-500">
+                      {name}
+                    </span>
+                  </span>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <MyDropdown
-        availableFilters={availableFilters}
-        sortMethod={sortMethod}
-        appliedFilters={appliedFilters}
-        filterMethod={filterMethod}
-      />
     </div>
   );
 };
@@ -178,11 +182,8 @@ function MyDropdown({
           className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-zinc-900 px-3 py-2 text-sm font-semibold shadow-sm ring-1 
         ring-inset ring-gray-300 hover:bg-black text-zinc-200"
         >
-          Options
-          <ChevronDownIcon
-            className="h-5 w-5 text-gray-300"
-            aria-hidden="true"
-          />
+          Add Genres
+          <ChevronDownIcon className={("h-5 w-5 text-gray-300" aria-hidden="true")} />
         </Menu.Button>
       </div>
 
@@ -199,20 +200,20 @@ function MyDropdown({
           <div className="py-1">
             {availableFilters.map((name, index) => {
               return (
-                <Menu.Item key={name + "genre dropdown"}>
+                <Menu.Item key={name + 'genre dropdown'}>
                   {({ active }) => (
                     <Link
                       href={{
-                        pathname: "/",
+                        pathname: '/',
                         query: {
                           sort: sortMethod,
-                          filters: [...appliedFilters, name].join(","),
+                          filters: [...appliedFilters, name].join(','),
                           filterMethod: filterMethod,
                         },
                       }}
                       className={clsx(
-                        active ? "bg-black text-green-400" : "text-zinc-200",
-                        "block px-4 py-2 text-sm cursor-pointer text-center w-full"
+                        active ? 'bg-black text-green-400' : 'text-zinc-200',
+                        'block px-4 py-2 text-sm cursor-pointer text-center w-full'
                       )}
                     >
                       {name}
