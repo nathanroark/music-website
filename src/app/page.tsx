565df32ac81802f35575f1783539d24f47d38eda
Nathan Roark <nathanroark@protonmail.com>
2023-08-07 21:25:34 -0500

    gave up on search bar for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,23 @@
-import AlbumsPage from "@/components/AlbumsPage";
+import Footer from "@/components/Footer";
+import PostPreview from "../components/PostPreview";
+import getPostMetadata from "@/server/getPostMetadata";
+
 export default function Home() {
+  const postMetadata = getPostMetadata();
+  const postPreviews = postMetadata.map((post) => (
+    <PostPreview key={post.slug} {...post} />
+  ));
+
   return (
-    <>
-      <AlbumsPage />
-    </>
+    <div className="flex justify-center items-center flex-col pt-[3rem]">
+      <div className="flex justify-center">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <div className="grid gap-2 md:gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 ">
+            {postPreviews}
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </div>
   );
 }
-
-
-//import Footer from "@/components/Footer";
-//import PostPreview from "../components/PostPreview";
-//import getPostMetadata from "@/server/getPostMetadata";
-//
-//export default function Home() {
-//  const postMetadata = getPostMetadata();
-//  const postPreviews = postMetadata.map((post) => (
-//    <PostPreview key={post.slug} {...post} />
-//  ));
-//
-//  return (
-//    <div className="flex justify-center items-center flex-col pt-[3rem]">
-//      <div className="flex justify-center">
-//        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-//          <div className="grid gap-2 md:gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 ">
-//            {postPreviews}
-//          </div>
-//          <Footer />
-//        </div>
-//      </div>
-//    </div>
-//  );
-//}
